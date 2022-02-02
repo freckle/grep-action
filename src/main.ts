672d90af0880220da7ea4c3034c41f222cd61880
patrick brisbin <pbrisbin@gmail.com>
2022-02-02 16:23:26 -0500

    Actually call run()

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,53 @@ import * as github from "./github";
 import type { GrepResult } from "./grep";
 import { grep } from "./grep";
 
-export async function run() {
+async function getFiles(
+  onlyChanged: boolean,
+  changedFiles: string[],
+  pattern: Pattern
+): Promise<string[]> {
+  if (onlyChanged) {
+    const matchers = pattern.paths.map((p) => new Minimatch(p));
+
+    return changedFiles.filter((file) => {
+      matchers.some((m) => m.match(file));
+    });
+  } else {
+    const globber = await glob.create(pattern.paths.join("\n"));
+    return await globber.glob();
+  }
+}
+
+function toAnnotation(pattern: Pattern, result: GrepResult): Annotation {
+  switch (result.tag) {
+    case "match":
+      return {
+        path: result.path,
+        start_line: result.line,
+        end_line: result.line,
+        start_column: null,
+        end_column: null,
+        annotation_level: pattern.level,
+        message: pattern.message,
+        title: pattern.title,
+        raw_details: result.input,
+      };
+    case "nomatch":
+      return {
+        path: "unknown",
+        start_line: 0,
+        end_line: 0,
+        start_column: null,
+        end_column: null,
+        annotation_level: "notice",
+        message: `Grep output not parsable: ${result.message}`,
+        title: null,
+        raw_details: result.input,
+      };
+  }
+}
+
+async function run() {
   try {
     core.startGroup("Inputs");
 
@@ -71,48 +117,4 @@ export async function run() {
   }
 }
 
-async function getFiles(
-  onlyChanged: boolean,
-  changedFiles: string[],
-  pattern: Pattern
-): Promise<string[]> {
-  if (onlyChanged) {
-    const matchers = pattern.paths.map((p) => new Minimatch(p));
-
-    return changedFiles.filter((file) => {
-      matchers.some((m) => m.match(file));
-    });
-  } else {
-    const globber = await glob.create(pattern.paths.join("\n"));
-    return await globber.glob();
-  }
-}
-
-function toAnnotation(pattern: Pattern, result: GrepResult): Annotation {
-  switch (result.tag) {
-    case "match":
-      return {
-        path: result.path,
-        start_line: result.line,
-        end_line: result.line,
-        start_column: null,
-        end_column: null,
-        annotation_level: pattern.level,
-        message: pattern.message,
-        title: pattern.title,
-        raw_details: result.input,
-      };
-    case "nomatch":
-      return {
-        path: "unknown",
-        start_line: 0,
-        end_line: 0,
-        start_column: null,
-        end_column: null,
-        annotation_level: "notice",
-        message: `Grep output not parsable: ${result.message}`,
-        title: null,
-        raw_details: result.input,
-      };
-  }
-}
+run();
